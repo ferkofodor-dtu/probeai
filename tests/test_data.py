2e53f17cd05928c5aec38c6fc648157af931f2fd
Ferenc <s220356@dtu.dk>
2024-01-19 00:45:28 +0100

    test only the model

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,13 +1,13 @@
-from tests import _PATH_DATA
-from probeai.data.make_dataset import load_probeai
-import torch
-import numpy as np
+# from tests import _PATH_DATA
+# from probeai.data.make_dataset import load_probeai
+# import torch
+# import numpy as np
 
 
-def test_data():
-    train, test = load_probeai()
-    assert len(train) == 108
-    assert len(test) == 92
-    assert len(set([train[i][0].shape for i in range(len(train))])) == 1
-    assert train[0][0].shape == torch.Size([1, 395, 70])
-    assert len(np.unique([train[i][1].numpy() for i in range(len(train))])) == 2
+# def test_data():
+#     train, test = load_probeai()
+#     assert len(train) == 108
+#     assert len(test) == 92
+#     assert len(set([train[i][0].shape for i in range(len(train))])) == 1
+#     assert train[0][0].shape == torch.Size([1, 395, 70])
+#     assert len(np.unique([train[i][1].numpy() for i in range(len(train))])) == 2
