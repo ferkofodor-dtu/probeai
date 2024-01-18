e0b899e415910bafd1279d38df573cb0034686be
Ferenc <s220356@dtu.dk>
2024-01-18 15:38:56 +0100

    bugfixes

--- probeai/train_model.py
+++ probeai/train_model.py
@@ -14,6 +14,9 @@ import wandb
 
 wandb.login()
 
+wandb.init(project="probeai")
+
+
 log = logging.getLogger(__name__)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -27,17 +30,17 @@ def make_loader(dataset, batch_size):
 
 def make(config):
     # Make the data
-    train, test = load_probeai(config.dataset_path)
-    train_loader = make_loader(train, batch_size=config.batch_size)
-    test_loader = make_loader(test, batch_size=config.batch_size)
+    train, test = load_probeai(config.train_conf['dataset_path'])
+    train_loader = make_loader(train, batch_size=config.train_conf['batch_size'])
+    test_loader = make_loader(test, batch_size=config.train_conf['batch_size'])
 
     # Make the model
-    model = MyNeuralNet(config.in_features, config.out_features).to(device)
+    model = MyNeuralNet(config.model_conf['in_features'], config.model_conf['out_features']).to(device)
 
     # Make the loss and optimizer
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(
-        model.parameters(), lr=config.learning_rate)
+        model.parameters(), lr=config.train_conf['learning_rate'])
     
     return model, train_loader, test_loader, criterion, optimizer
 
@@ -51,7 +54,7 @@ def train_batch(images, labels, model, optimizer, criterion):
     images, labels = images.to(device), labels.to(device)
     
     # Forward pass ➡
-    outputs = model(images)
+    outputs, _ = model(images)
     loss = criterion(outputs, labels)
     
     # Backward pass ⬅
@@ -65,7 +68,6 @@ def train_batch(images, labels, model, optimizer, criterion):
 
 def test(model, test_loader, criterion, epoch, save_model=False):
     model.eval()
-    test_losses = []
     # Run the model on some test examples
     with torch.no_grad():
         running_loss = 0
@@ -78,40 +80,35 @@ def test(model, test_loader, criterion, epoch, save_model=False):
             correct += (predicted == labels).sum().item()
             loss = criterion(log_ps, labels)
             running_loss += loss.cpu().item()
-        test_losses.append(running_loss / len(test_loader))
 
         print(f"Accuracy of the model on the {total} " +
               f"test images: {correct / total:%}")
         
-        wandb.log({"test_accuracy": correct / total})
+    return running_loss / len(test_loader)
+        
+        # wandb.log({"test_accuracy": correct / total})
 
     if save_model:
         # Save the model in the exchangeable ONNX format
         torch.onnx.export(model, images, f"model_{epoch}.onnx")
-        wandb.save(f"model_{epoch}.onnx")
+        # wandb.save(f"model_{epoch}.onnx")
 
 
 # @click.command()
 # @click.option("--epochs", default=5, help="number of epochs to train for")
 # @click.option("--batch", default=8, help="batch size to use for training")
 # @click.option("--lr", default=1e-3, help="learning rate to use for training")
-@hydra.main(version_base="1.1", config_path="config", config_name="defaults.yaml")
+@hydra.main(version_base=None, config_path="config", config_name="defaults.yaml")
 def train(config):
     log.info(OmegaConf.to_yaml(config))
     log.info("Training day and night")
 
-    # Get the config
-    modelc_ = config.model_conf
-    hyperpms = config.train_conf
-    
-    # append two configs
-    modelc = OmegaConf.merge(modelc_, hyperpms)
 
-    torch.manual_seed(hyperpms['seed'])
+    torch.manual_seed(config.train_conf['seed'])
 
-    log.info(f"Dataset path: {hyperpms['dataset_path']}")
+    log.info(f"Dataset path: {config.train_conf['dataset_path']}")
 
-    model, train_loader, test_loader, criterion, optimizer = make(modelc)
+    model, train_loader, test_loader, criterion, optimizer = make(config)
 
     # Profiling
     prof = torch.profiler.profile(
@@ -137,13 +134,13 @@ def train(config):
     # used when outputting for tensorboard
     )
 
-    total_batches = len(train_loader) * hyperpms["n_epochs"]
+    total_batches = len(train_loader) * config.train_conf["n_epochs"]
     example_ct = 0  # number of examples seen
     batch_ct = 0
 
     train_losses, test_losses = [], []
     # Training loop
-    for e in range(hyperpms['n_epochs']):
+    for e in range(config.train_conf['n_epochs']):
         running_loss = 0
         with prof:
             for images, labels in train_loader:
@@ -154,7 +151,8 @@ def train(config):
             train_losses.append(running_loss / len(train_loader))
 
             # Log the losses
-            test_losses = test(model, test_loader, criterion, e)
+            test_loss = test(model, test_loader, criterion, e)
+            test_losses.append(test_loss)
 
             log.info(f"Epoch: {e}, Training loss: {train_losses[-1]:.4f}, Validation loss: {test_losses[-1]:.4f}")
     
@@ -174,5 +172,5 @@ def train(config):
 
 
 if "__main__" == __name__:
-    with wandb.init(project="probeai", entity="ferkofodor-dtu"):
-        train()
+    
+    train()
